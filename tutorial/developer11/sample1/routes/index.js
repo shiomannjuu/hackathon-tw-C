191ba72c456610e53435a2b8b90d25c78ad0c6f6
Saito19 <fatildtdt@gmail.com>
2019-04-03 20:26:20 +0900

    committed in 2019/04/03 20:26:20

--- tutorial/developer11/sample1/routes/index.js
+++ tutorial/developer11/sample1/routes/index.js
@@ -8,12 +8,12 @@ const router = express.Router();
 
 // ルートパス「/」にアクセスした場合の処理
 router.get('/', function(request, response) {
-    response.send('Hello World!');
+    response.send('Hello Wood!');
 });
 
 // 「/rakus」にアクセスした場合の処理
 router.get('/rakus', function(request, response) {
-    response.send('Hello Rakus!');
+    response.send('Hello Rakus1!');
 });
 
 // ルーティングオブジェクトを外部ファイルへエクスポートする
