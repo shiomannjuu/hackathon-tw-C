44e834af98af3e00be5f837e4430e4dfe36699e2
y-swarow <junyapc.arde.12@gmail.com>
2019-04-10 20:51:58 +0900

    committed in 2019/04/10 20:51:58

--- chatapp/public/javascripts/exit.js
+++ chatapp/public/javascripts/exit.js
@@ -3,14 +3,15 @@
 // 退室メッセージをサーバに送信する
 function exit() {
     // ユーザ名取得
-    const userName = '';
+    const userName = $('#userName').val();
     // 退室メッセージイベントを送信する
+    socket.emit( 'sendExitEvent', userName );
 
     // 退室
     location.href = '/';
 }
 
 // サーバから受信した退室メッセージを画面上に表示する
-socket.on('', function (data) {
-    $('#thread').prepend('<p>' + '</p>');
+socket.on('sendExitEvent', function (userName) {
+    $('#thread').prepend('<p>' + userName + 'さんが退室しました。' + '</p>');
 });
--- chatapp/sockets/exit.js
+++ chatapp/sockets/exit.js
@@ -2,7 +2,7 @@
 
 module.exports = function (socket) {
     // 退室メッセージをクライアントに送信する
-    socket.on('', function (data) {
-
+    socket.on('sendExitEvent', function (userName) {
+      socket.broadcast.emit( 'sendExitEvent', userName );
     });
 };
